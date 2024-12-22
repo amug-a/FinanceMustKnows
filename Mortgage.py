2ab896a076534c04bb80bbba729c870802a7dec8
Otito Amuga <otitoamuga@gmail.com>
2024-12-22 07:42:15 -0600

    correction to output monthly interest rate symbol $ -> %

--- Mortgage.py
+++ Mortgage.py
@@ -7,4 +7,4 @@ monthly_interest_rate = annual_interest_rate / 12
 number_of_payments = loan_term_years * 12
 monthly_payment = loan_amount * (monthly_interest_rate * (1 + monthly_interest_rate)**number_of_payments) / ((1 + monthly_interest_rate)**number_of_payments - 1)
 print(f"Your monthly mortgage payment is: ${monthly_payment:.2f}")
-print(f"Your monthly interest rate is: ${monthly_interest_rate:.6f} on your current Principal")
+print(f"Your monthly interest rate is: {monthly_interest_rate:.6f}% on your current Principal")
