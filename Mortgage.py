a66605b25ce22bea9b4946b21681379b8a243ba6
Otito Amuga <otitoamuga@gmail.com>
2024-12-22 07:39:01 -0600

    refactored for inputs

--- Mortgage.py
+++ Mortgage.py
@@ -1,9 +1,10 @@
-# Mortgage 
+# Fixed Mortage Payment Based On Principal At Sign Up
 
-loan_amount = 270000
-annual_interest_rate = 0.04
-loan_term_years = 30
+loan_amount = float(input("Enter Loan Amount After Down Payment: "))
+annual_interest_rate = float(input("Enter Annual Interest Rate: "))/100
+loan_term_years = int(input("Enter Compound Years: "))
 monthly_interest_rate = annual_interest_rate / 12
 number_of_payments = loan_term_years * 12
 monthly_payment = loan_amount * (monthly_interest_rate * (1 + monthly_interest_rate)**number_of_payments) / ((1 + monthly_interest_rate)**number_of_payments - 1)
 print(f"Your monthly mortgage payment is: ${monthly_payment:.2f}")
+print(f"Your monthly interest rate is: ${monthly_interest_rate:.6f} on your current Principal")
